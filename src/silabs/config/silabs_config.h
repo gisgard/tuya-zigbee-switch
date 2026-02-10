2952297b9e40673fd62757041744149977b03dbd
gisgard <54429466+gisgard@users.noreply.github.com>
2026-02-10 14:05:31 +0700

    Delete src/silabs directory

--- src/silabs/config/silabs_config.h
+++
@@ -1,8 +0,0 @@
-// <o VERSION_STR>
-#define VERSION_STR               "0.0.0"
-
-// <o NVM_MIGRATIONS_VERSION>
-#define NVM_MIGRATIONS_VERSION    1
-
-// <o DEFAULT_CONFIG>
-#define DEFAULT_CONFIG            "device;device;"
